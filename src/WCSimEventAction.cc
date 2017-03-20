5348363c34ea2e646da092d07af49051d59a237e
Mahdi Taani <mahdi.taani@ed.ac.uk>
2017-03-20 14:52:12 +0000

    Revert "Update WCSimEventAction.cc"
    This reverts commit b8197d6e6232ab8480802c13641828c23df5539b.

--- src/WCSimEventAction.cc
+++ src/WCSimEventAction.cc
@@ -378,7 +378,7 @@ void WCSimEventAction::EndOfEventAction(const G4Event* evt)
 	(WCSimTrajectory*)((*(evt->GetTrajectoryContainer()))[i]);
 
       if (trj->GetCharge() != 0.)
- 	trj->DrawTrajectory();
+ 	trj->DrawTrajectory(50);
     }
 
    G4cout << " Filling Root Event " << G4endl;
